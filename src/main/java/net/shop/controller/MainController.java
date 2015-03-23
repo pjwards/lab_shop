ca9a2bb007f69e59a1e7fcf5900b8e40d5247658
cbajs12 <jisung_j@yahoo.com>
2015-03-23 12:40:27 +0900

    add log4j

--- src/main/java/net/shop/controller/MainController.java
+++ src/main/java/net/shop/controller/MainController.java
@@ -1,14 +1,17 @@
 package net.shop.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class MainController {
-
+	//private Logger log = Logger.getLogger(this.getClass());
+	
 	@RequestMapping("/main/main.do")
 	public String showMain() throws Exception{
+		//log.debug("Test Logger");
 		return "/main/main";
 	}
 }
